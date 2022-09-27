daa74ea5945e407cb83c391e2dad6183c73d7421
Syntax <muhammadmasoom1001@gmail.com>
2022-09-27 11:04:46 +0100

    i made it better

--- logn.py
+++ logn.py
@@ -1,20 +1,19 @@
 import os, binascii, hashlib
 
+if not os.path.isfile('usr.txt'): # If usr.txt does not exist it writes to it
+    with open('usr.txt', 'w') as f:
+        f.write('[]')
+
 def hash_password(passwo):
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-    pwdhash = hashlib.pbkdf2_hmac('sha512', passwo.encode('utf-8'),
-                                salt, 100000)
+    pwdhash = hashlib.pbkdf2_hmac('sha512', passwo.encode('utf-8'),salt, 100000)
     pwdhash = binascii.hexlify(pwdhash)
-    return (salt + pwdhash).decode('ascii')
-#encrypts password
+    return (salt + pwdhash).decode('ascii') # encrypts password    
 
 def verify_password(stored_password, provided_password):
     salt = stored_password[:64]
     stored_password = stored_password[64:]
-    pwdhash = hashlib.pbkdf2_hmac('sha512',
-                                  provided_password.encode('utf-8'),
-                                  salt.encode('ascii'),
-                                  100000)
+    pwdhash = hashlib.pbkdf2_hmac('sha512',provided_password.encode('utf-8'),salt.encode('ascii'),100000)
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
     return pwdhash == stored_password
 #encrypts given password in same way as stored one and checks if they are the same
@@ -38,34 +37,20 @@ def login(ln):
         login(ln)
 
 def password(pw):
-    dg = False
-    up = False
-    lo = False
-    ln = False
-    if len(pw) >= 8:
-        ln = True
-    passw = list(pw)
-    for a in passw:
-        if a.isdigit() and dg == False:
-            dg = True
-        elif a.isupper() and up == False:
-            up = True
-        elif a.islower() and lo == False:
-            lo = True
-
-    if dg and up and lo and ln:
-        return False
+    # dg = False
+    # up = False
+    # lo = False
+    # ln = False    -- Very unnecessary code, but I'm keeping it here for now. 
+    if len(pw) <= 8:
+        print('PASSWORD MUS BE AT LEAST 8 LETTERS LONG'); return False
+    elif pw.isdigit():
+        print('PASSWORD MUST CONTAIN A LETTER'); return False
+    elif pw.isupper() :
+        print('PASSWORD MUST CONTAIN A LOWERCASE LETTER'); return False
+    elif pw.islower():
+        print("PASSWORD MUST CONTAIN AN UPPERCASE LETTER"); return False
     else:
-        if dg is False:
-            print('PASSWORD MUST CONTAIN A NUMBER')
-        if up is False:
-            print('PASSWORD MUST CONTAIN A CAPITAL LETTER')
-        if lo is False:
-            print('PASSWORD MUST CONTAIN A LOWERCASE LETTER')
-        if ln is False:
-            print('PASSWORD MUST HAVE AT LEAST 8 CHARACTERS')
-        print('\n')
-        return True
+        print('\n'); return True
 
 def new(user, pw, listnm, filename):
     listnm.append([user, hash_password(pw)])
@@ -74,7 +59,7 @@ def new(user, pw, listnm, filename):
     filename.write(str(listnm))
 
 def create(listname, filename):
-    us = input("\nEnter your username here ")
+    us = input("\nEnter your username here ")        
     allowed = True
     for name in range(len(listname)):
         if us == listname[name][0]:
@@ -82,9 +67,10 @@ def create(listname, filename):
     #checks if username is valid, and only then lets you enter a password
     if allowed is True:
         pw = input('Enter your password here ')
-        while password(pw):
+        while password(pw) == False:
             pw = input('Enter your password here ')
         new(us, pw, listname, filename)
+        print('Account Created')
     elif allowed is False:
         print('This username already exists. please choose another one')
         create(listname, filename)
@@ -96,7 +82,6 @@ def main():
         if ch == 1:
             login(users)
             # TODO: add things to do here once logged in
-
         elif ch == 2:
             create(users, n)
         else:
